3d8ab12997ca41b5367bb3777c1b923aa4dc4d45
Nuko <binaryee89@gmail.com>
2026-06-25 16:13:15 +0800

    6/25

--- C++/lc3737_SubarraysWithMajorityI/main.cpp
+++ C++/lc3737_SubarraysWithMajorityI/main.cpp
@@ -6,42 +6,21 @@ using namespace std;
 
 class Solution {
 public:
-    int maxNumberOfBalloons(string text) {
-        int arr[5] = {0};
-        
-        for (auto& c : text) {
-            switch(c) {
-                case 'b'  :
-                    arr[0]++;
-                    break;
-                
-                case 'a'  :
-                    arr[1]++;
-                    break;
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) {
+                    cnt++;
+                }
 
-                case 'n'  :
-                    arr[2]++;
-                    break;
-
-                case 'l'  :
-                    arr[3]++;
-                    break;
-
-                case 'o'  :
-                    arr[4]++;
-                    break;
-            
-                default :
-                    break;
+                int gap = j - i;
+                if (cnt > gap / 2 + gap % 2) {
+                    ans++;
+                }
             }
-        }
-        
-        arr[3] /= 2;
-        arr[4] /= 2;
-
-        int ans = INT_MAX;
-        for (auto& a : arr) {
-            ans = min(ans, a);
         }
         return ans;
     }
